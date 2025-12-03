7547605b911e9655574b536e818bd5e1fd4ec445
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-12-03 23:44:17 +0530

    initialize the project folders

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import "../global.css";
+
 import { StatusBar } from "expo-status-bar";
 
 // Prevent the splash screen from auto-hiding
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,19 +2,6 @@ import { useEffect, useState } from "react";
 import { Redirect } from "expo-router";
 import useAuthStore from "@/stores/authStore";
 
-<<<<<<< HEAD
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-600">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-=======
 const Home = () => {
   const { isSignedIn, checkAuthStatus } = useAuthStore();
   const [loading, setLoading] = useState(true);
@@ -33,7 +20,6 @@ const Home = () => {
     <Redirect href="/(root)/(tabs)/home" />
   ) : (
     <Redirect href="/(auth)/onBoard1" />
->>>>>>> 3e57d2b444f0ef4279d0188a6aa9e67cb5fdf39b
   );
 };
 
